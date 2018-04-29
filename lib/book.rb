b40ad3eb3c96c24bac371e2177046832bb45864c
Monica Marziano <mmarziano1@gmail.com>
2018-04-29 09:46:06 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,17 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :author, :page_count
   attr_reader :title
 
   GENRES = []
+  
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end 
+  
+  def genre 
+    @genre
+  end  
   
   def initialize(title)
     @title = title
